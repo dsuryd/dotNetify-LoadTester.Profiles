0456a07d6515fa70244c95d73b0e8ee8b4c19320
Dicky Suryadi <dsuryd@gmail.com>
2021-03-02 15:30:57 -0800

    Initial revision.

---
+++ TestRunner/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.IO;
+using System.Threading.Tasks;
+using DotNetify.LoadTester;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace TestRunner
+{
+   internal class Program
+   {
+      private static async Task Main(string[] args)
+      {
+         IConfigurationRoot configuration = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+            .Build();
+
+         ILoggerFactory loggerFactory = LoggerFactory.Create(configure => configure.AddConfiguration(configuration.GetSection("Logging")).AddConsole());
+
+         await LoadTestRunner.RunAsync(args, loggerFactory);
+      }
+   }
+}
